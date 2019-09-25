c113c63b213e70fe062d69b7b4bbdbe337a101c9
Brunna Rocha <brunna.17rocha03@gmail.com>
2019-09-25 16:26:48 -0300

    Subindo os passo 3, 4 e 5 de uma só vez, porque estava travando e o Golias me ajudou

--- semana-3/aula-3-classes/blog.js
+++ semana-3/aula-3-classes/blog.js
@@ -1,10 +1,16 @@
+function limpaCampo(){
+    document.getElementById("titulo").value = "";
+    document.getElementById("autor").value = "";
+    document.getElementById("conteudo").value = "";
+    document.getElementById("imagem").value = "";
+}
+
 class Post {
-    constructor(titulo, autor, conteudo){
+    constructor(titulo, autor, conteudo, imagem){
         this.titulo = titulo;
         this.autor = autor;
         this.conteudo = conteudo;
-        this.conteudo = imagem
-        
+        this.imagem = imagem 
     }
 
     buscaPostFormatado(){
@@ -13,20 +19,22 @@ class Post {
     }
 
 }
+    let listaDePost = []
+    function publicarOPost(){
+    const titulo = document.getElementById("titulo").value;
+    const autor = document.getElementById("autor").value;
+    const conteudo = document.getElementById("conteudo").value;
+    const imagem = document.getElementById("imagem").value;
+    const post = new Post (titulo, autor, conteudo, imagem);
+    listaDePost.push (post);
+    console.log(listaDePost);
+    limpaCampo();
+    }
 
-    const post1 = new Post("Post A", "Post A", "Post A", "https://picsum.photos/200/200?v=1");
-    const post2 = new Post("Post B", "Post B", "Post B", "https://picsum.photos/200/200?v=2");
-    const post3 = new Post("Post C", "Post C", "Post C", "https://picsum.photos/200/200?v=3");
-    const post4 = new Post("Post D", "Post D", "Post D", "https://picsum.photos/200/200?v=4");
-    const post5 = new Post("Post E", "Post E", "Post E", "https://picsum.photos/200/200?v=5");
-    const post6 = new Post("Post F", "Post F", "Post F", "https://picsum.photos/200/200?v=6");
-
-    const playlist = [post1, post2, post3, post4, post5, post6];
-
-    const listaDePosts = document.getElementById("listaDePosts");
-
-    for(let post of playlist){
-        listaDePosts.innerHTML += "<li>" + post.buscaPostFormatado() + "</li>";
-        listaDePosts.innerHTML += "<img src=" + post.imagem + ">";
+    for(let publicacao of listaDePost) {
+        document.getElementById("listaDePost").innerHTML += "<div>" + "<h1>" + publicacao.titulo + "</h1>" + "</div>"
+        document.getElementById("listaDePost").innerHTML += "<div>" + "<h1>" + publicacao.autor + "</h1>" + "</div>"
+        document.getElementById("listaDePost").innerHTML += "<div>" + "<h1>" + publicacao.conteudo + "</h1>" + "</div>"
+        document.getElementById("listaDePost").innerHTML += "<div>" + "<h1>" + publicacao.imagem + "</h1>" + "</div>"
     }
 
