741ec93402632716d490a3affac437beebf38f54
Brunna Rocha <brunna.17rocha03@gmail.com>
2019-09-30 16:24:00 -0300

    Finalizando o exercício da tarde com ajuste de imagens, inserção de menu, inserção de mais duas imagens, bem como a criação de uma div que englobou o menu lateral e as imagens e um ajuste no corpo do HTML feito no CSS.

--- semana-4/aula-1-React/futuretube/src/App.js
+++ semana-4/aula-1-React/futuretube/src/App.js
@@ -16,50 +16,58 @@ function App() {
             <input name="text" placeholder="Buscar" />
         </form>
       </header>
+        <div id="aside-e-section">
+            <aside>
+                <ul class="menulateralesquerdo">
+                    <li>Início</li> 
+                    <li>Em alta</li> 
+                    <li>Inscrições</li> 
+                    <li>Originais</li> 
+                    <hr/>
+                    <li>Biblioteca</li> 
+                    <li>Histórico</li>
+                </ul>
+            </aside>
 
-      <aside>
-          <ul class="menulateralesquerdo">
-              <li>Início</li> 
-              <li>Em alta</li> 
-              <li>Inscrições</li> 
-              <li>Originais</li> 
-              <hr/>
-              <li>Biblioteca</li> 
-              <li>Histórico</li>
-          </ul>
-      </aside>
-
-      <section class="primeiraSessao">
-          <div>
-              <img src= {"https://picsum.photos/200/200?v=" + new Date().getTime()} alt="imagem de um barquinho" />
-              <a href="videoUm.html">
-                  <p> Imagem 1 </p>
-              </a>
-          </div>
-          <div>
-              <img src= {foto2} alt="imagem de um porto" />
-              <p>Quase um paraíso</p>
-          </div>
-          <div>
-              <img src= {foto3} alt="imagem de uma noite estrelada" />
-              <p>Noite feliz</p>
-          </div>
-          <div>
-              <img src= {foto4} alt="imagem de cabos de internet" />
-              <p>Cabo, Desligo!</p>
-          </div>
-          <div>
-              <img src= {foto5} alt="imagem de um limão cheio de poros" />
-              <p>Poros Everywhere!</p>
-          </div>
-          <div>
-              <img src= {foto6} alt="imagem de uma floresta na Rússia" />
-              <p>Hello Russia!</p>
-          </div>
-      </section>
-    
+            <section class="primeiraSessao">
+                <div>
+                    <img src= {"https://picsum.photos/200/200?v=algumacoisa" + new Date().getTime()} alt="imagem de um barquinho" />
+                    <a href="videoUm.html">
+                        <p> Imagem 1 </p>
+                    </a>
+                </div>
+                <div>
+                    <img src= {foto2} alt="imagem de um porto" />
+                    <p>Quase um paraíso</p>
+                </div>
+                <div>
+                    <img src= {foto3} alt="imagem de uma noite estrelada" />
+                    <p>Noite feliz</p>
+                </div>
+                <div>
+                    <img src= {foto4} alt="imagem de cabos de internet" />
+                    <p>Cabo, Desligo!</p>
+                </div>
+                <div>
+                    <img src= {foto5} alt="imagem de um limão cheio de poros" />
+                    <p>Poros Everywhere!</p>
+                </div>
+                <div>
+                    <img src= {foto6} alt="imagem de uma floresta na Rússia" />
+                    <p>Hello Russia!</p>
+                </div>
+                <div>
+                    <img  src= {"https://picsum.photos/200/200?v=outracoisa" + new Date().getTime()} />
+                    <p>Imagem 4!</p>
+                </div>
+                <div>
+                    <img src= {"https://picsum.photos/200/200?v=coisinha" + new Date().getTime()} />
+                    <p>Imagem 5!</p>
+                </div>
+            </section>
+        </div>
       <footer>
-          <h1>Oi! Eu moro no Footer!</h1>
+        <h1>Oi! Eu moro no Footer!</h1>
       </footer>
     </div>
   );
