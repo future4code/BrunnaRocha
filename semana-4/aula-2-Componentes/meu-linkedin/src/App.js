88af16d83d6629d4165dd6d049c75465d50ee4a3
Brunna Rocha <brunna.17rocha03@gmail.com>
2019-10-01 14:57:54 -0300

    Criando as primeiras funções para trabalhar no projeto.

--- semana-4/aula-2-Componentes/meu-linkedin/src/App.js
+++ semana-4/aula-2-Componentes/meu-linkedin/src/App.js
@@ -4,15 +4,50 @@ import { BigCard } from '.components/BigCard/BigCard';
 import { ImageButton } from '.components/ImageButton/ImageButton';
 import { PageButton } from '.components/PageButton/PageButton';
 import { SmallCard } from '.components/SmallCard/SmallCard';
+import { PageSection } from './components/PageSection/PageSection';
+import facebook from './Imagens/facebook.png'
 
+
+
+const bigCardApresentacao = {
+  // image: , INSERIR UMA FOTO PESSOAL E FAZER O IMPORT LÁ EM CIMA 
+  title: 'Brunna Rocha',
+  paragraph: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim necessitatibus doloremque quam, et nobis error excepturi eveniet autem vero asperiores sunt voluptas alias obcaecati, animi ex voluptatum vitae ea impedit.'
+}
+​
+const smallCardEmail = {
+  // image: , INSERIR UMA FOTO DE UM ENVELOPE E FAZER O IMPORT LÁ EM CIMA 
+  label: 'Email: ',
+  email: 'brunna.17rocha03@gmail.com'
+}
+
+const smallCardEndereco = {
+  label: 'Endereço: '
+  endereco: 'Rua do Programador, 7'
+}
 
 function App() {
   return (
-    <div className="App">
-      <BigCard />
-      <ImageButton />
-      <PageButton />
-      <SmallCard />
+    <div className="Container">
+
+      <BigCard image={bigCardApresentacao.image} title={bigCardApresentacao.title} paragraph={bigCardApresentacao.paragraph} />
+
+      <SmallCard image={smallCardEmail.image} label={smallCardEmail.label} email={smallCardEmail.email} />
+
+      <SmallCard image={smallCardEndereco.image} label={smallCardEndereco.label} endereco={smallCardEmail.email} />
+      
+      <div>
+        <ImageButton />
+      </div>
+      
+      <div>
+        <PageButton />
+      </div>
+
+      <div>
+        <PageSection />
+      </div>
+      
     </div>
   );
 }
--- semana-4/aula-2-Componentes/meu-linkedin/src/Components/ImageButton/ImageButton.js
+++ semana-4/aula-2-Componentes/meu-linkedin/src/Components/ImageButton/ImageButton.js
@@ -1,3 +1,11 @@
 import React from 'react';
 import './ImageButton.css';
 import PropTypes from 'prop-types';
+
+export function PageButton() {
+    return (
+      <div id="ver-mais-btn">
+        <button type="button">Ver mais</button>
+      </div>
+    )
+  }
