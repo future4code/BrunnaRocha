4e6316c9b5cc7ed454c5e21483d4414c8c6a318c
Brunna Rocha <brunna.17rocha03@gmail.com>
2019-10-07 12:33:39 -0300

    Aplicando as primeiras implementações

--- semana-5/aula-1-lifecicle-components/exercicio-lista-de-tarefas/src/App.js
+++ semana-5/aula-1-lifecicle-components/exercicio-lista-de-tarefas/src/App.js
@@ -4,22 +4,63 @@ import './App.css';
 
 function App() {
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div class="container">
+          <form action="#" id="task-form">
+          <label>Insira uma tarefa:</label>
+          <input type="text" name="task" id="task">
+          </form>
+          <select name="diaDaSemana" id="diaDaSemana">
+              <option value="segunda">Segunda-Feira</option>
+              <option value="terca">Terça-Feira</option>
+              <option value="quarta">Quarta-Feira</option>
+              <option value="quinta">Quinta-Feira</option>
+              <option value="sexta">Sexta-Feira</option>
+              <option value="sabado">Sábado</option>
+              <option value="domingo">Domingo</option>
+          </select>
+
+          <button onclick="creatTask();">Criar Tarefa!</button>
+      </div>
+
+      <hr>
+
+      <main>
+        <div id="segunda">
+            Segunda-feira
+            <ul class="content"></ul>
+        </div>
+        <div id="terca">
+            Terça-Feira
+            <ul class="content"></ul>
+        </div>
+        <div id="quarta">
+            Quarta-Feira
+            <ul class="content"></ul>
+        </div>
+        <div id="quinta">
+            Quinta-Feira
+            <ul class="content"></ul>
+        </div>
+        <div id="sexta">
+            Sexta-Feira
+            <ul class="content"></ul>
+        </div>
+        <div id="sabado">
+            Sábado
+            <ul class="content"></ul>
+        </div>
+        <div id="domingo">
+            Domingo
+            <ul class="content"></ul>
+        </div>
+      </main>
+
+  
     </div>
+    
   );
 }
 
