d80f977089ba095db8c82706e38fa1aa026f230c
Brunna Rocha <brunna.17rocha03@gmail.com>
2019-10-07 15:58:32 -0300

    Continuo tentando

--- semana-5/aula-1-lifecicle-components/exercicio-lista-de-tarefas/src/App.js
+++ semana-5/aula-1-lifecicle-components/exercicio-lista-de-tarefas/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Tasks  from "./components/tasks/Tasks.js";
+import Tasks  from "./components/criartarefa/Tasks.js.js";
 import styled from "styled-components";
 
 const Container = styled.section`
@@ -8,12 +8,38 @@ const Container = styled.section`
     background-color: white;
 `;
 
-function App() {
-    return (
-        <Container>
-            <Tasks />
-        </Container>
-    );
+class App extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            listaDeTarefas: [],
+        }
+    }
+
+
+    render() {
+        return (
+            <Container>
+                <p>TAREFAS</p>
+            
+                <Cabeçalho>
+                    <span>Em curso</span>
+                    <button></button>
+                </Cabeçalho> 
+                <ul>
+                {/* Aqui entrarão as tarefas que não foram concluídas ainda */}
+                </ul>  
+                
+                <Cabeçalho>
+                    <span>Concluídas</span>
+                </Cabeçalho> 
+                <ul>
+                    {/* Aqui entrarão todas as tarefas concluídas */}
+                </ul>
+            </Container>
+        );
+    }
 }
 
 export default App;
---
+++ semana-5/aula-1-lifecicle-components/exercicio-lista-de-tarefas/src/components/tarefa/tarefa.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+
+
+export function tarefa() {
+        return (
+            <li>
+                {this.props.tarefaCriada}
+            </li>
+        );
+}
