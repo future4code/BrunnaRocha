a0292964d0fcdce0da856f4353943076e0fa6cb7
Brunna Rocha <brunna.17rocha03@gmail.com>
2019-10-08 14:53:27 -0300

    criando os inputs com as perguntas e as duas listas de possíveis respostas prontas

--- semana-5/aula-2-renderizacao-condicional/exercicio-future-form/src/App.js
+++ semana-5/aula-2-renderizacao-condicional/exercicio-future-form/src/App.js
@@ -4,14 +4,46 @@ import './App.css';
 function App() {
   return (
     <div>
-      <span>Nome</span>
-      <input type="text" name="nome" id="nome"/> 
-      <span>Idade</span>
-      <input type="number" name="idade" id="idade"/>
-      <span>E-mail</span>
-      <input type="text" name="email" id="email"/>
-      <span>Grau de escolaridade</span>
-      <input type="text" name="escolaridade" id="escolaridade"/>
+      <div className="primeiraEtapa">
+        <span>Nome</span>
+        <input type="text" name="nome" id="nome"/> 
+
+        <span>Idade</span>
+        <input type="text" name="idade" id="idade"/>
+
+        <span>E-mail</span>
+        <input type="text" name="email" id="email"/>
+
+        <span>Grau de escolaridade</span>
+        <input type="text" name="escolaridade" id="escolaridade"/>
+        <ul>
+          <li>Ensino Médio Incompleto</li>
+          <li>Ensino Médio Completo</li>
+          <li>Ensino Superior Incompleto</li>
+          <li>Ensino Superior Completo</li>
+        </ul>
+      </div>
+
+      <div className="SegundaEtapa">
+        <span>Qual o curso?</span>
+        <input type="text" name="curso" id="curso"/>
+
+        <span>Qual a unidade de ensino?</span>
+        <input type="text" name="unidadeensino" id="unidadeensino"/>
+      </div>
+
+      <div className="terceiraEtapa">
+        <span>Por que você não terminou um curso de graduação?</span>
+        <input type="text" name="graduacaoincompleta" id="graduacaoincompleta"/>
+
+        <span>Você fez algum curso complementar?</span>
+        <input type="text" name="cursocomplementar" id="cursosomplementar"/>
+        <ul>
+          <li>Curso técnico</li>
+          <li>Curso de inglês</li>
+          <li>Não fiz nem curso complementar</li>
+        </ul>
+      </div>
     </div>
   );
 }
