a0aa4ae7736c88e07afbf3480b0e822c32a79309
Brunna Rocha <brunna.17rocha03@gmail.com>
2019-10-16 14:01:44 -0300

    Layout do component Signup: Done

--- semana-6/aula-2-API/futureusers/src/App.js
+++ semana-6/aula-2-API/futureusers/src/App.js
@@ -1,36 +1,27 @@
-import React, { Component } from 'react'
-import UsersRegistered from './components/usersregistered/UsersRegistered'
-import NewUsers from './components/newusers/NewUsers'
+import React from 'react';
+import Signup from './components/Signup.js';
+import Styled from 'styled-components'; 
 import axios from 'axios';
-import Styled from 'styled-components' 
 
-//Criando uma variável que recebe objetos estáticos
-const usersList = [{
-  user1: {
-    name: "Amanda",
-    email: "amanda123@gmail.com"
-  },
-  user2: {
-    name: "Brunna",
-    email: "brunna123@gmail.com"
-  },
-  user3: {
-    name: "Daniel",
-    email: "daniel123@gmail.com"
-  },
-}]
 
-// Passamos a props com a lista de usuários (usersList) que está na variável, que recebe ela mesma.
-//Após passar as props, ela foi chamada pela função "function  UsersRegistered" que a recebeu e portanto ficou "function  UsersRegistered(props)"
-class App extends Component {
-  render() {
+const userToSignup = {
+    name: "Emmanuel",
+    email: "Godwithus@gmail.com"
+  }
+
+const userList = [
+  {
+    id: "id1",
+    name: "Emmanuel"
+  }
+]  
+
+function App() {
     return (
       <div>
-        <UsersRegistered usersList={usersList}/>
-        <NewUsers/>
+        <Signup/>
       </div>
-    )
-  }
+    );
 }
 
 
---
+++ semana-6/aula-2-API/futureusers/src/components/Signup.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+
+const SignupContainer = styled.div `
+    width: 40%;
+    display: flex;
+    flex-direction: column;
+` 
+
+ class Signup extends React.Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             
+        }
+    }
+    
+    render() {
+        return (
+            <SignupContainer>
+                <input placeholder= "Nome" type="text"/>
+                <input placeholder= "Email" type="text"/>
+                <button>Salvar</button>
+            </SignupContainer>
+        )
+    }
+}
+
+export default Signup;
