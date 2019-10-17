b577f47717c0855cc5b778e4baaa8262806d2d19
Brunna Rocha <brunna.17rocha03@gmail.com>
2019-10-17 15:29:54 -0300

    Criando duas funções que ao serem chamadas pelos inputs, mudam de acordo com o que o usuário inputa nos campos

--- semana-6/aula-4-Server-e-Browser/BoredAPI/boredapiproject/src/Components/BoredAPI/index.js
+++ semana-6/aula-4-Server-e-Browser/BoredAPI/boredapiproject/src/Components/BoredAPI/index.js
@@ -12,17 +12,27 @@ class BoredAPI extends React.Component {
         }
     }
 
+    handleOnChangeActivity = (event) => {
+        this.setState({currentActivityType: event.target.value})
+    }
+
+    handleOnChangeParticipantsCount = (event) => {
+        const newValue = Number(event.target.value);
+        this.setState({currentParticipantsNumber: newValue})
+    }
+
     render () {
         return(
             <div>
                 <input 
-                    name="activity" 
-                    type="text" 
+                    onChange={this.handleOnChangeActivity}
+                    placeholder="Activity"
+                    name="activity"  
                     value={ this.state.currentActivityType }/>
 
                 <input 
+                    onChange={this.handleOnChangeParticipantsCount}
                     name="participantsCount" 
-                    type="number" 
                     value={ this.state.currentParticipantsNumber }/>
             </div>
         )
