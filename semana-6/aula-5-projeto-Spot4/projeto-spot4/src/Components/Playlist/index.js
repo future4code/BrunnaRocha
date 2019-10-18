a33542caaed780d88dff18376b5321e414d78fed
Brunna Rocha <brunna.17rocha03@gmail.com>
2019-10-18 12:34:47 -0300

    Criando os imports, a classe e o construtor para iniciar o projeto

---
+++ semana-6/aula-5-projeto-Spot4/projeto-spot4/src/Components/Playlist/index.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import Style from 'styled-components';
+import axios from 'axios';
+
+
+ class Playlist extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+             
+        }
+    }
+    
+    render() {
+        return (
+            <div>
+              <h1>OLÁ, ESTOU AQUI</h1>  
+            </div>
+        )
+    }
+}
+
+export default Playlist;
