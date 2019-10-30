6ed06fe8cf3232bfc70c628ec49b90c41a720e1a
Brunna Rocha <brunna.17rocha03@gmail.com>
2019-10-30 15:57:22 -0300

    Controlando o input no estado

--- semana-8/aula-1-Redux/missao-newton-react-boilerplate-master/src/components/TaskConfig/TaskCard.js
+++ semana-8/aula-1-Redux/missao-newton-react-boilerplate-master/src/components/TaskConfig/TaskCard.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function TaskCard() {
+export default function TaskCard(props) {
     return (
         <div>
-            <p>Tarefas</p>
+            <p>{props.task.name}</p>
             <button>Completar</button>
             <button>Deletar</button>
         </div>
--- semana-8/aula-1-Redux/missao-newton-react-boilerplate-master/src/components/TaskConfig/TaskConfig.js
+++ semana-8/aula-1-Redux/missao-newton-react-boilerplate-master/src/components/TaskConfig/TaskConfig.js
@@ -5,27 +5,31 @@ import { connect } from "react-redux";
  class TaskConfig extends React.Component {
 	 constructor(props) {
 		 super(props)
-	 
 		 this.state = {
-			  
+			input: "", 
 		}
 	 }
+
+	handleOnChange = (e) => {
+		this.setState({input:e.target.value})	
+	}
 	 
     render() {
 		console.log(this.props.allTasks)
+		const allTasks = this.props.allTasks.map((element) => {
+			return <TaskCard task={element}/>
+		})
         return (
             <div>
                 <h1>4TASK</h1>
                 <label htmlFor="textToDo">O que precisa ser feito?</label>
-                <input type="text"/>
+                <input value={this.state.input} onChange={this.handleOnChange} type="text"/>
                 <button>Marcar todas como completas</button>  
                 <button>Todas</button>
                 <button>Pendentes</button>
                 <button>Completas</button> 
                 <div>
-                    {() => {this.props.allTasks.map((element) => {
-						return <TaskCard task={element}/>
-					})}}
+                    {allTasks}
                 </div>             
             </div>
         )
