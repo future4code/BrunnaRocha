3cc0f0827888a1ebe1f30e7302ad4bab63c0b708
Brunna Rocha <brunna.17rocha03@gmail.com>
2019-11-01 12:59:35 -0300

    Alterando o nome na API, apenas

--- semana-8/aula-4-AstroMatch/astro-match/astro-match/src/actions/profiles.js
+++ semana-8/aula-4-AstroMatch/astro-match/astro-match/src/actions/profiles.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const clearSwipes = () => async (dispatch) => {
-	await axios.put('https://us-central1-missao-newton.cloudfunctions.net/astroMatch/darvas/clear')
+	await axios.put('https://us-central1-missao-newton.cloudfunctions.net/astroMatch/brunna/clear')
 }
