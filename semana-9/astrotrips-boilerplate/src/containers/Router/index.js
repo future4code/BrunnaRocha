76f905be13367b47932549616e419f5dd73c97e4
Brunna Rocha <brunna.17rocha03@gmail.com>
2019-11-05 10:49:45 -0300

    Rotas definidas e mapeadas: Done.

--- semana-9/astrotrips-boilerplate/src/containers/Router/index.js
+++ semana-9/astrotrips-boilerplate/src/containers/Router/index.js
@@ -4,8 +4,14 @@ import { Switch, Route } from "react-router-dom";
 import LoginPage from "../LoginPage";
 
 const routes = {
-  root: "/"
-  // Outras rotas aqui
+  root: "/",
+  home: "/home",
+  formTrips: "/application-form",
+  createTrips: "/trips/create",
+  listTrips: "/trips/list",
+  detailsTrips: "/trips/details",
+
+
 };
 
 function Router(props) {
@@ -13,6 +19,11 @@ function Router(props) {
     <ConnectedRouter history={props.history}>
       <Switch>
         <Route path={routes.root} component={LoginPage} />
+        <Route path={routes.home} component={HomePage} />
+        <Route path={routes.formTrips} component={ApplicationForm} />
+        <Route path={routes.createTrips} component={HomePage} />
+        <Route path={routes.listTrips} component={listTripsPage} />
+        <Route path={routes.detailsTrips} component={detailsTripsPage} />
       </Switch>
     </ConnectedRouter>
   );
