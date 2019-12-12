8637f7e8ad0c49c752843d996ebb342ec47d17c3
Brunna Rocha <brunna.17rocha03@gmail.com>
2019-12-12 16:52:43 -0300

    Apagando arquivos desnecessários

---
+++ semana-14/aula-4-solid/build/error.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Error {
+    constructor(text, date) {
+        this.date = date;
+        this.text = text;
+    }
+}
+exports.Error = Error;
+//# sourceMappingURL=error.js.map
---
+++ semana-14/aula-4-solid/build/post.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Post {
+    constructor(name, text, date) {
+        this.name = name;
+        this.text = text;
+        this.date = date;
+    }
+}
+//# sourceMappingURL=post.js.map
