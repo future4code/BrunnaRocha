b0b218287c8a9e2baae144c48a900ea91309bd84
Brunna Rocha <brunna.17rocha03@gmail.com>
2019-12-13 12:21:20 -0300

    Enum Specialty criado

---
+++ semana-14/F4 System/src/specialty.ts
@@ -0,0 +1,12 @@
+
+
+export enum Specialty {
+    NEWTON = "Newton",
+    HTML = "Html",
+    CSS = "Css",
+    JS = "Js",
+    REACT = "React",
+    REDUX = "Redux",
+    BACK = "Back",
+    OO = "Oo"
+}
--- semana-14/F4 System/src/teacher.ts
+++ semana-14/F4 System/src/teacher.ts
@@ -1,13 +1,14 @@
-import { User } from "./user"
-import * as moment from "moment"
+import { User } from "./user";
+import * as moment from "moment";
+import { Specialty } from "./specialty"
 
 export class Teacher implements User {
-    specialty: string // tipo: specialty - ENUM
+    specialty: Specialty;
     name: string;    
     email: string;
     birthDate: moment.Moment;
     
-    constructor(name: string, email: string, birthDate: moment.Moment, specialty: string){
+    constructor(name: string, email: string, birthDate: moment.Moment, specialty: Specialty){
         this.name = name;
         this.email = email;
         this.birthDate = birthDate;
