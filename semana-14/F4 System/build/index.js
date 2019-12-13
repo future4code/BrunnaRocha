af45c95fcfcb7317d874e36624dd2da9dc3907ac
Brunna Rocha <brunna.17rocha03@gmail.com>
2019-12-13 17:02:05 -0300

    Classe gerenciadora de cadastramento criada

--- semana-14/F4 System/build/index.js
+++ semana-14/F4 System/build/index.js
@@ -1,9 +1,3 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const student_1 = require("./student");
-const webMission_1 = require("./webMission");
-const newton = new webMission_1.Webmission("Newton", "05-02-2019", "04-02-2020", [], []);
-console.log(newton);
-const marieCurie = new student_1.Student("Marie Curie", "mariecurie@uol.com", 3, 12, 1978, newton);
-console.log(marieCurie);
 //# sourceMappingURL=index.js.map
--- semana-14/F4 System/src/index.ts
+++ semana-14/F4 System/src/index.ts
@@ -4,8 +4,3 @@ import { Specialty} from "./specialty";
 import { Webmission } from "./webMission";
 
 
-const newton: Webmission = new Webmission("Newton", "05-02-2019", "04-02-2020", [], [])
-console.log(newton)
-
-const marieCurie: any = new Student("Marie Curie", "mariecurie@uol.com", 3,12,1978, newton)
-console.log(marieCurie)
