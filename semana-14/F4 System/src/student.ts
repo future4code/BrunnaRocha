33dde0a19352f9a28ee65c70ff0481b9c4ab2065
Brunna Rocha <brunna.17rocha03@gmail.com>
2019-12-13 11:48:35 -0300

    Inserindo o tipo Mission na propriedade Team do arquivo Student.Ts

--- semana-14/F4 System/src/student.ts
+++ semana-14/F4 System/src/student.ts
@@ -1,13 +1,14 @@
 import { User } from "./user"
 import * as moment from "moment"
+import { Mission } from "./mission";
 
 export class Student implements User {
-    team: string // tipo: Mission
+    team: Mission;
     name: string;    
     email: string;
     birthDate: moment.Moment;
     
-    constructor(name: string, email: string, birthDate: moment.Moment, team: string){
+    constructor(name: string, email: string, birthDate: moment.Moment, team: Mission){
         this.name = name;
         this.email = email;
         this.birthDate = birthDate;
