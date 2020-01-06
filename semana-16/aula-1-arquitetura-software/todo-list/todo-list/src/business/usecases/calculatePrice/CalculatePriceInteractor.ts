f8b3a68c6db4822cd46c46f2e13202d8444cbbe1
Brunna Rocha <brunna.17rocha03@gmail.com>
2020-01-06 15:29:18 -0300

    Criando a database do cliente

--- semana-16/aula-1-arquitetura-software/todo-list/todo-list/src/business/usecases/calculatePrice/CalculatePriceInteractor.ts
+++ semana-16/aula-1-arquitetura-software/todo-list/todo-list/src/business/usecases/calculatePrice/CalculatePriceInteractor.ts
@@ -1,34 +1,7 @@
-import {User} from '../../entities/User'
+import { ClientOrder } from '../../entities/ClientOrder'
 import {generateRandomId} from '../../utils/generateRandomId'
-import {UserDatabase} from '../../../data/UserDatabase'
+import {CalculatePriceDatabase} from '../../../data/CalculatePriceDatabase'
 
 export class CalculatePriceInteractor {
-  private userDatabase: UserDatabase
-
-  constructor() {
-    this.userDatabase = new UserDatabase()
+  private calculatePriceDatabase: CalculatePriceDatabase
   }
-
-  async execute(createUserInput: CreateUserInput): Promise<CreateUserOutput> {
-    const newUser = new User(generateRandomId(), createUserInput.name, createUserInput.nickname, createUserInput.email, createUserInput.birthdate)
-
-    await this.userDatabase.createUser(newUser)
-
-    return {
-      id: newUser.getId(),
-      email: newUser.getEmail()
-    }
-  }
-}
-
-export interface CreateUserInput {
-  name: string
-  nickname: string
-  email: string
-  birthdate: Date
-}
-
-export interface CreateUserOutput {
-  id: string
-  email: string
-}
