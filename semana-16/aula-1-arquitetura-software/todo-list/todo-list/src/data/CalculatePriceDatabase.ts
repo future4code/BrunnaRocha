75e1cb47536042ddcf6ce1bb7443c72eba6598a4
Brunna Rocha <brunna.17rocha03@gmail.com>
2020-01-07 15:29:44 -0300

    Confeccionando a database

--- semana-16/aula-1-arquitetura-software/todo-list/todo-list/src/data/CalculatePriceDatabase.ts
+++ semana-16/aula-1-arquitetura-software/todo-list/todo-list/src/data/CalculatePriceDatabase.ts
@@ -1,38 +1,41 @@
 import knex from 'knex'
-import {Task} from '../business/entities/Task'
+import {ClientOrder} from '../business/entities/ClientOrder'
+
+interface ClientOrderModel {
+  id: string
+  name: string
+  email: string
+  paper: string
+  size: string
+  borderType: string
+  borderSize: string
+}
 
 export class CalculatePriceDatabase {
     private connection: knex
-    private taskEntityMapper: TaskEntityMapper
   
     constructor() {
       this.connection = knex({
         client: 'mysql',
         connection: {
           host: 'ec2-18-229-236-15.sa-east-1.compute.amazonaws.com',
-          user: 'darvas',
+          user: 'brunna',
           password: 'teste',
           database: 'aulas_arquitetura'
         }
       })
-  
-      this.taskEntityMapper = new TaskEntityMapper()
     }
-
-    async createTask(task: Task) {
-        await this.connection('tasks').insert(this.taskEntityMapper.entityToModel(task))
-      }
     
-      async getTasksByUserId(userId: string): Promise<Task[]> {
-        const results: TaskModel[] = await this.connection.raw(`
-          SELECT * FROM tasks
-          JOIN users ON users.id=tasks.responsible_user_id
-          WHERE users.id="${userId}";
-        `).then(res => {
-          return res[0]
-        })
-    
-        return results.map((result) => this.taskEntityMapper.modelToEntity(result))
+      async saveOrder(clientOrder: ClientOrder): Promise<void> {
+        const databaseModel: ClientOrderModel = {
+          id: clientOrder.getId(),
+          name: clientOrder.
+          email: clientOrder.
+          paper: clientOrder.
+          size: clientOrder.
+          borderType: clientOrder.
+          borderSize: clientOrder.
+        }
       }
     }
     
