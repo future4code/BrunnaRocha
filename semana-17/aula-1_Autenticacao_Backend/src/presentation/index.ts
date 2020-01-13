cb1767571d7a56794e0f6f03525d1af44dd1db02
Brunna Rocha <brunna.17rocha03@gmail.com>
2020-01-13 16:01:27 -0300

    Camada de presentaqtion criada

--- semana-17/aula-1_Autenticacao_Backend/src/presentation/index.ts
+++ semana-17/aula-1_Autenticacao_Backend/src/presentation/index.ts
@@ -4,4 +4,28 @@ import express, {Request, Response} from 'express'
 const app = express()
 app.use(express.json()) // Linha mágica (middleware)
 
-export default app
+const getTokenFromHeaders = (headers: any): string => {
+    return (headers["auth"] as string) || "";
+};
+
+app.post("/login", async (req: Request, res: Response) => {});
+
+app.get("/getMyInformation", async (req: Request, res: Response) => {});
+
+app.post("/signup", async (req: Request, res: Response) => {});
+
+app.post("/changePassword", async (req: Request, res: Response) => {});
+
+app.get("/getAllUsers", async (req: Request, res: Response) => {
+    try {
+        const getAllUsersUC = new getAllUsersUC(new UserDataBase());
+        const result = await getAllUsersUC.execute();
+        res.status(200).send(result);
+    } catch (err) {
+        res.status(400).send({
+            errorMessage: err.message
+        });
+    }
+});
+
+export default app;
