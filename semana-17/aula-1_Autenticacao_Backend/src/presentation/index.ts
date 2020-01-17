14854618a0f4f1800afb7ac6c36cfa67d0066f17
Brunna Rocha <brunna.17rocha03@gmail.com>
2020-01-17 15:33:30 -0300

    Criando o arquivo JWTAuthService.ts

--- semana-17/aula-1_Autenticacao_Backend/src/presentation/index.ts
+++ semana-17/aula-1_Autenticacao_Backend/src/presentation/index.ts
@@ -1,4 +1,8 @@
 import express, {Request, Response} from 'express'
+import { LoginUC } from '../business/usecases/auth/Login';
+import { UserDatabase } from '../data/userDatabase';
+import { GetLoggedUserInformationUC } from '../business/usecases/users/GetLoggedUserInformation';
+import { CreateUserUC } from '../business/usecases/users/CreateUserUC';
 
 
 const app = express()
@@ -8,11 +12,62 @@ const getTokenFromHeaders = (headers: any): string => {
     return (headers["auth"] as string) || "";
 };
 
-app.post("/login", async (req: Request, res: Response) => {});
+app.post("/login", async (req: Request, res: Response) => {
+    try{
+        const loginUC = new LoginUC(
+            new UserDatabase(),
+            new BcryptService(),
+            new JwtAuthService()
+        )
 
-app.get("/getMyInformation", async (req: Request, res: Response) => {});
+        const result = await loginUC.execute({
+            email: req.body.email,
+            password: req.body.password
+        })
 
-app.post("/signup", async (req: Request, res: Response) => {});
+        res.send(result)
+
+    } catch(err) {
+        res.status(400).send({
+            message: err.message
+        })
+    }
+});
+
+app.get("/getMyInformation", async (req: Request, res: Response) => {
+    try {
+        const getLoggedUserInformationUC = new GetLoggedUserInformationUC(
+            new JwtAuthService(),
+            new UserDatabase()
+        )
+        const token = getTokenFromHeaders(req.headers)
+        const result = await getLoggedUserInformationUC.execute(token)
+            res.send(result).status(200)
+    } catch(err) {
+        res.status(400).send({
+            errorMessage: err.message
+        });
+    }
+});
+
+app.post("/signup", async (req: Request, res: Response) => {
+    try {
+        const createUserUC = new CreateUserUC(
+            new UserDatabase(),
+            new BcryptService(),
+            new V4IdGenerator()
+        );
+        const result = await createUserUC.execute({
+            email: req.body.email,
+            passwaord: req.body.password
+        });
+        res.status(200).send(result)
+    } catch (err) {
+        res.status(400).send({
+            errorMessage: err.message
+        });
+    }
+});
 
 app.post("/changePassword", async (req: Request, res: Response) => {});
 
