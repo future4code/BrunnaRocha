11fcebf9cad0881ef909936e988f24b3f64eab59
Brunna Rocha <brunna.17rocha03@gmail.com>
2020-01-17 14:58:12 -0300

    Ajustando o arquivo UserDataBase.ts

--- semana-17/aula-1_Autenticacao_Backend/src/data/userDatabase.ts
+++ semana-17/aula-1_Autenticacao_Backend/src/data/userDatabase.ts
@@ -1,28 +1,29 @@
 import { UserGateway } from "../business/gateways/user/UserGateway"
-import { User } from "../entities/User"
+import { User } from "../business/entities/User"
 import knex from "knex";
 
 export class UserDatabase implements UserGateway {
     private connection = knex({
-        client: "mysql",
+        client: 'mysql',
         connection: {
-            host: "localhost",
-            user: "root",
-            password: "jvg1015115",
-            database: "aulaJwt"
+            host: ' ec2-18-229-236-15.sa-east-1.compute.amazonaws.com',
+            user: 'brunna',
+            password: 'f0938b7263c683db91a52018a6b038e8',
+            database: 'brunna'
         }
     });
 
-    public async createUserTable(): Promise<void> {
-        await this.connection.raw(`DROP TABLE Users`);
-        await this.connection.raw(`CREATE TABLE Users(
-            id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-            name varchar(255) NOT NULL,
-            email varchar(255) NOT NULL UNIQUE,
-            age int NOT NULL,
-            password varchar(255) NOT NULL
-        )`);
-    }
+
+    // public async createUserTable(): Promise<void> {
+    //     await this.connection.raw(`DROP TABLE Users`);
+    //     await this.connection.raw(`CREATE TABLE Users(
+    //         id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    //         name varchar(255) NOT NULL,
+    //         email varchar(255) NOT NULL UNIQUE,
+    //         age int NOT NULL,
+    //         password varchar(255) NOT NULL
+    //     )`);
+    // }
 
     public async getUserById(id: string): Promise<User> {
         const query = await this.connection.raw(
@@ -51,5 +52,48 @@ export class UserDatabase implements UserGateway {
         if(!returnedUser) {
             throw new Error("User not found"); 
         }
+
+        return new User(
+            returnedUser.id,
+            returnedUser.email,
+            returnedUser.password,
+            returnedUser.name,
+            returnedUser.age 
+        );
+    }
+
+    public async getAllUsers(): Promise<User []> {
+        const query = this.connection.raw("SELECT * FROM Users;");
+        const usersFromDb = await query;
+        return usersFromDb[0]. map(
+            (user: any) =>
+                new User(user.id, user.email, user.password, user.name, user.age)
+        );
+    }
+
+    public async createUser(user: User): Promise<void> {
+        await this.connection
+        .insert({
+            id: user.getId(),
+            email: user.getEmail(),
+            password: user.getPassword()
+        })
+        .into("Users");
+    }
+
+    public async updatePassword(id: number, newPassword: string): Promise<void> {
+        await this.connection.raw(
+            `UPDATE Users SET password='${newPassword}' WHERE id=${id};`
+        );
+    }
+
+    async veryfyUserExists(id: string): Promise<boolean> {
+        const query = await this.connection.raw(
+            `SELECT * FROM Users WHERE id=${id};`
+        );
+
+        const returnedUser = query [0] [0];
+
+        return Boolean(returnedUser);
     }
 }
