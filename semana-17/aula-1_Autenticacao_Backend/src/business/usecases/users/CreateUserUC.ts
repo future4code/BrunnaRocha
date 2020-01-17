c6bc86563e49c339213cf6e5a1debc016e17997f
Brunna Rocha <brunna.17rocha03@gmail.com>
2020-01-17 14:00:29 -0300

    Ajustando o arquivo CreateUserUC.ts

--- semana-17/aula-1_Autenticacao_Backend/src/business/usecases/users/CreateUserUC.ts
+++ semana-17/aula-1_Autenticacao_Backend/src/business/usecases/users/CreateUserUC.ts
@@ -1,10 +1,37 @@
 import { UserGateway } from "../gateways/user/UserGateway"
 import { User } from "../../../entities/User"
+import { CryptographyGateway } from "../../gateways/crypt/CryptographyGateway"
+import { IdGeneratorGateway } from "../../gateways/user/IdGeneratorGateway"
 
 export class CreateUserUC {
-    constructor(private userGateway: UserGateway) {}
+    constructor(
+        private userGateway: UserGateway,
+        private cryptographyGateway: CryptographyGateway,
+        private idGenerator: IdGeneratorGateway
+        
+    ) {}
 
-    async execute(input: CreateUserUCInput): Promise<void> {}   
+    async execute(input: CreateUserUCInput): Promise<CreateUseUCOutput> {
+        const encryptedPassword = await this.cryptographyGateway.encrypt(
+            input.password
+        );
+
+        const user = new User(
+            this.idGenerator.generateId(),
+            input.email,
+            encryptedPassword,
+        )
+        try {
+            await this.userGateway.createUser(user)
+        } catch (err) {
+            console.log(err.message)
+            throw new Error("Ocorreu um erro!")
+        }
+
+        return {
+            message: "Usuário criado com sucesso!"
+        }
+    }   
 }
 
 export interface CreateUserUCInput {
