7ea7a47563fad77b6f24221a16efd590dbcc5358
Brunna Rocha <brunna.17rocha03@gmail.com>
2020-01-21 15:51:05 -0300

    Criando o UserDataBase para criar a tabela no Dbeaver e testar no Postman

--- semana-18/ts-jest-boilerplate-final/ts-jest-boilerplate-final/src/business/gateways/UserGateway.ts
+++
@@ -1,5 +0,0 @@
-import { User } from '../entities/User'
-
-export interface UserGateway{
-    registerUser(user: User): Promise<void>;
-}
--- semana-18/ts-jest-boilerplate-final/ts-jest-boilerplate-final/src/data/UserDatabase.ts
+++ semana-18/ts-jest-boilerplate-final/ts-jest-boilerplate-final/src/data/UserDatabase.ts
@@ -1,5 +1,64 @@
 import knex from 'knex';
+import { User } from '../business/entities/User';
+
 
 export class UserDatabase extends UserGateway {
+    private connection = knex({...
+    });
 
+
+    public async getUserById(id: string): Promise<User> {
+        const query = await this.connection.raw(
+            `SELECT * FROM user_FutureBook WHERE id='${id}';`
+        );
+
+        const returnedUser = query[0] [0];
+        if (!returnedUser) {
+            throw new Error("usuário não encontrado");
+        }
+
+        return new User(
+            returnedUser.id,
+            returnedUser.name,
+            returnedUser.email,
+            returnedUser.password
+        );
+    }
+
+    public async getUserByEmail (email: string): Promise<User> {
+        const query = await this.connection.raw(
+            `SELECT * FROM user_FutureBook WHERE email='${email}';`
+        );
+
+        const returnedUser = query [0] [0];
+        if(!returnedUser) {
+            throw new Error("Usuário não encontrado");
+        }
+        return new User(
+            returnedUser.id,
+            returnedUser.name,
+            returnedUser.email,
+            returnedUser.password
+        );
+    }
+
+    public async createUser(user: User): Promise<void> {
+        await this.connection
+        .insert({
+            id: user.getId(),
+            name: user.getName(),
+            email: user.getEmail(),
+            password: user.getPassword()
+        })
+        .into("user_FutureBook")
+    }
+
+    async verifyUserExists(id: string): Promise<boolean> {
+        const query = await this.connection.raw(
+            `SELECT * FROM use_FutureBook WHERE id=${id};`
+        );
+        const returnedUser = query [0] [0];
+
+        return Boolean(returnedUser);
+    }
 }
